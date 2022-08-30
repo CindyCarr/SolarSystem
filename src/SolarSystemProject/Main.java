fe201e3e8d0c019426924c92fe6d4c6b56ab7758
CindyCarr <cindyparedes001@gmail.com>
2022-08-30 13:02:02 -0400

    solar system ready to be deployed

--- src/SolarSystemProject/Main.java
+++ src/SolarSystemProject/Main.java
@@ -2,6 +2,11 @@ package SolarSystemProject;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        solarSystem inception=new solarSystem();
+        inception.sun=1;
+        inception.moon=4;
+        inception.planets=2;
+
+        System.out.println("the basic structure is ready");
     }
 }
